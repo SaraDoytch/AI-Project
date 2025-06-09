3779dd83ef87a20619001f677cb18d9dfb09d260
saraDoitch <sara0527656182@gmail.com>
2025-06-09 20:49:05 +0300

    change user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+   firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // hashed password
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
